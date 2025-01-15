ea7e63bee866e092d41ec5eb17881b873eb6d809
Leonardo-Sanchez <lsc3107@gmail.com>
2025-01-15 12:15:22 -0500

    Add files via upload

---
+++ Pato.java
@@ -0,0 +1,16 @@
+
+package Controller;
+
+/**
+ *
+ * @author UsuarioPC
+ */
+public class Pato {
+    
+    public static void main (String[] args){
+        int num=15;
+        
+       System.out.print("El pato tiene" + num + "años");
+    }
+    
+}
